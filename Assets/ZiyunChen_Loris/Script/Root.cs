833aa47dcfdd8c56c9bd9e82c99201ccf8c7e22a
Bryan Chiu <bryan.zhao@ansys.com>
2023-02-04 20:37:16 +0000

    roothealth

---
+++ Assets/ZiyunChen_Loris/Script/Root.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    int health;
+    void SetHealth(int h)
+    {
+        health = h;
+    }
+    void TakeDamage(int val)
+    {
+        health -= val;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
