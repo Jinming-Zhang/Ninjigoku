ac932a5b6626d1e2614afb95ed9f8e195e390a40
Ziyun Chen <chen.ziyun@northeastern.edu>
2023-02-05 13:01:37 -0800

    add health bar, player death animation

--- Assets/FinalDelivery/Script/Boss.cs
+++ Assets/FinalDelivery/Script/Boss.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Boss : MonoBehaviour
 {
     [SerializeField]
     float maxHealth;
 
+   
+    public AudioClip deathSound; 
     public float health;
     bool dead;
+   
     // Start is called before the first frame update
     void Start()
     {
+       
         health = maxHealth;
         dead = false;
     }
+
+   
     public void TakeDamage(float dmg)
     {
         health = Mathf.Max(0, health - dmg);
+        
         if (health == 0)
         {
             if (!dead)
-            {
+            {   
+                AudioSystem.Instance.PlaySFX(deathSound);
                 dead = true;
                 GameStatus.Instance.PlayerWin();
                 Die();
