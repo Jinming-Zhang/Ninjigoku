99b582e63001c2f89dbb6b2ea962cb9688528dc1
Ziyun Chen <chen.ziyun@northeastern.edu>
2023-02-04 22:21:49 -0800

    add bullet material

--- Assets/Player_REEEEEED/PlayerMovement.cs
+++ Assets/Player_REEEEEED/PlayerMovement.cs
@@ -36,28 +36,29 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.forward * speedMove * Time.fixedDeltaTime;
-            //pos.x = pos.f + speedMove * Time.fixedDeltaTime;
+            //transform.position += transform.forward * speedMove * Time.fixedDeltaTime;
+            pos.x = pos.x + speedMove * Time.fixedDeltaTime;
 
             GetComponent<PlayAudio>().playFootStepsAudio();
         }
         if (Input.GetKey(KeyCode.S))
         {
-            //pos.x = pos.x - speedMove * Time.fixedDeltaTime;
-            transform.position -= transform.forward * speedMove * Time.fixedDeltaTime;
+            pos.x = pos.x - speedMove * Time.fixedDeltaTime;
+            //transform.position -= transform.forward * speedMove * Time.fixedDeltaTime;
 
             GetComponent<PlayAudio>().playFootStepsAudio();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            //pos.z = pos.z + speedMove * Time.fixedDeltaTime;
-            transform.position -= transform.right * speedMove * Time.fixedDeltaTime;
+            pos.z = pos.z + speedMove * Time.fixedDeltaTime;
+            //transform.position -= transform.right * speedMove * Time.fixedDeltaTime;
 
             GetComponent<PlayAudio>().playFootStepsAudio();
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += transform.right * speedMove * Time.fixedDeltaTime;
+            pos.z = pos.z - speedMove * Time.fixedDeltaTime;
+            //transform.position += transform.right * speedMove * Time.fixedDeltaTime;
 
             GetComponent<PlayAudio>().playFootStepsAudio();
         }
