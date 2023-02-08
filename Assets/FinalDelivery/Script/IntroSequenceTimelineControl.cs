632d6e06a0b60535b209153ec4e20da43c3b7684
Jinming-Zhang <jinming.zhang47@gmail.com>
2023-02-08 14:53:43 -0500

    lose lose lose

--- Assets/FinalDelivery/Script/IntroSequenceTimelineControl.cs
+++ Assets/FinalDelivery/Script/IntroSequenceTimelineControl.cs
@@ -9,9 +9,9 @@ using Unity.VisualScripting;
 
 [RequireComponent(typeof(PlayableDirector))]
 public class IntroSequenceTimelineControl : MonoBehaviour
-{   
+{
     public static event Action onEnd;
-    
+
     PlayableDirector director;
     PlayerCollision player;
     Dialogue dialogue;
@@ -19,23 +19,25 @@ public class IntroSequenceTimelineControl : MonoBehaviour
     public GameObject Slider;
     [SerializeField] CinemachineVirtualCamera virtualCamera;
     // Start is called before the first frame update
+    [SerializeField]
+    GameStatus gameStatus;
     private void Awake()
     {
         director = GetComponent<PlayableDirector>();
-        dialogue= FindObjectOfType<Dialogue>();
+        dialogue = FindObjectOfType<Dialogue>();
         director.played += DisablePlayerControl;
         director.stopped += EnablePlayerControl;
     }
     void Start()
     {
         Slider.SetActive(false);
-        //StartCoroutine(StartCutScence());
         dialogue = FindObjectOfType<Dialogue>();
         player = FindObjectOfType<PlayerCollision>();
     }
     private void Update()
     {
-        if (!cutsceneisEnded && Input.GetKeyUp(KeyCode.Escape)) {
+        if (!cutsceneisEnded && Input.GetKeyUp(KeyCode.Escape))
+        {
             player.isDead = false;
             director.Stop();
             virtualCamera.Priority = 10;
@@ -46,26 +48,22 @@ public class IntroSequenceTimelineControl : MonoBehaviour
     }
     void DisablePlayerControl(PlayableDirector p)
     {
-        
-        Debug.LogWarning("start playing");
-        //var player = FindObjectOfType<PlayerCollision>();
         player.isDead = true;
         StartCoroutine(ChangeCameraSettings());
         dialogue.canClick = false;
         cutsceneisEnded = false;
-
     }
-    void EnablePlayerControl(PlayableDirector p) {
-  
-        //var player = FindObjectOfType<PlayerCollision>();
-        //player.isDead = false;
+
+    void EnablePlayerControl(PlayableDirector p)
+    {
         dialogue.canClick = true;
         Slider.SetActive(true);
+        gameStatus.shouldCount = true;
     }
 
-    IEnumerator ChangeCameraSettings() {
+    IEnumerator ChangeCameraSettings()
+    {
         yield return new WaitForSeconds(2);
         onEnd.Invoke();
-        
     }
 }
--- Assets/WolvesAreEssential/GameMode/GameStatus.cs
+++ Assets/WolvesAreEssential/GameMode/GameStatus.cs
@@ -7,9 +7,21 @@ public class GameStatus : MonoBehaviour
     private static GameStatus instance;
     public static GameStatus Instance => instance;
     public bool freeze = false;
+    [SerializeField]
+    float autoLoseInSec;
+    float loseCounter;
+    [SerializeField]
+    TMPro.TextMeshProUGUI counterUI;
+
+    public bool shouldCount;
+    public PlayerCollision player;
 
     private void Awake()
     {
+        loseCounter = autoLoseInSec;
+        shouldCount = false;
+        counterUI.gameObject.SetActive(shouldCount);
+
         if (!instance)
         {
             instance = this;
@@ -20,10 +32,36 @@ public class GameStatus : MonoBehaviour
         }
     }
 
-    public void PlayerLose()
+    private void Update()
     {
+        counterUI.gameObject.SetActive(shouldCount);
+        if (shouldCount)
+        {
+            loseCounter -= Time.deltaTime;
+        }
+
+        if (loseCounter < 0)
+        {
+            shouldCount = false;
+            PlayerLose();
+        }
+        else
+        {
+            updateCounterUI();
+        }
 
     }
+    public void updateCounterUI()
+    {
+        counterUI.text = loseCounter.ToString("0.00");
+    }
+
+    public void PlayerLose()
+    {
+        player.isDead = true;
+        LoseCanvas.Instance.Show();
+    }
+
     public void PlayerWin()
     {
         WinCanvas.Instance.Show();
